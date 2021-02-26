631170500ce955e5ab9f62cf4fdf7f7fde777dda
BurstingMango <18407691+BurstingMango@users.noreply.github.com>
2021-02-26 12:14:22 -0500

    added datastructures folder with linkedlist

---
+++ datastructures/linkedlist/java/linkedlistTest.java
@@ -0,0 +1,9 @@
+class linkedlistTest {
+    public static void main(String[] args) {
+        linkedlist list = new linkedlist();
+        list.add(45);
+        list.add(8);
+        list.add(22);
+        System.out.println(list);
+    }
+}
---
+++ datastructures/linkedlist/java/test.java
@@ -0,0 +1,12 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        LinkedList list = new LinkedList<Integer>();
+        list.add(5);
+        System.out.println(list);
+        
+    }
+}
